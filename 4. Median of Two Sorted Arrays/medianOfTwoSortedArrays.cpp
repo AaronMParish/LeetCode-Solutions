4a1ba93b2f4ef8406394ec87c12b25ef3d672bf0
AaronMParish <aaron-m-parish@proton.me>
2024-02-04 22:44:18 -0600

    added median

--- 4. Median of Two Sorted Arrays/medianOfTwoSortedArrays.cpp
+++ 4. Median of Two Sorted Arrays/medianOfTwoSortedArrays.cpp
@@ -1,68 +1,73 @@
+//Implementation of Manacher's algorithm.
+#include <string>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        //script uses smallest list for algorithm
-        if (nums2.size() < nums1.size())
-            return findMedianSortedArrays(nums2, nums1);
+    // Fills string with special character '#' 
+    // to make string even. (Note: New size = N*2 + 1)
+    string fillString(const string s) {
+        string temp = "$";
 
-        int size1 = nums1.size(),   //size of smallest list
-            size2 = nums2.size(),   //size of largest list
-            start = 0,              //start of binary search range
-            end = size1,            //end of binary search range
-            partitionA = 0,         //pivot point for nums1
-            partitionB = 0,         //pivot point for nums2
-            maxLeftA = 0,           // These four vars check values beside pivot
-            minRightA = 0,          // ie:
-            maxLeftB = 0,           // [x] | [] 
-            minRightB = 0;          // [] | [x]
+        for( char c : s )
+            temp += string(1, c) + '#';
 
-        //run loop until match is found
-        while (start <= end)
-        {
-            /*
-            We find where to place the partition in both nums1 and nums2.
-            Each time we dont find valid median, remove half of nums1 from selection
-            partitionA (nums1) = 0 - nums1.size
-            partitionB (nums2) is size of both arrays / 2 - partitionA.
-            max/min A and B are the two numbers on each side of the partition.
-                We want to find if nums1.left < nums2.right and nums2.left < nums1.right
-                    nums1  w | x
-                    nums2  y | z
-                    w < z, y < x
-                if we find the correct partitions, we can find the median from w, x, y, z.
-            */
-            partitionA = (start + end) / 2,
-                partitionB = (size1 + size2 + 1) / 2 - partitionA,
-                maxLeftA = (partitionA <= 0) ? INT_MIN : nums1[partitionA - 1];
-            minRightA = (partitionA >= size1) ? INT_MAX : nums1[partitionA];
-            maxLeftB = (partitionB <= 0) ? INT_MIN : nums2[partitionB - 1];
-            minRightB = (partitionB >= size2) ? INT_MAX : nums2[partitionB];
+        temp = temp.substr(0, temp.size() - 1) + "@";
 
-            // Median found
-            if (maxLeftA <= minRightB && maxLeftB <= minRightA)
-            {
-                //if the merged array's size is even
-                if ((size1 + size2) % 2 == 0)
-                {
-                    //find average between the two middle values
-                    return (min(minRightA, minRightB) + max(maxLeftA, maxLeftB)) / 2.0;
-                }
-                else
-                {
-                    //return highest middle
-                    return max(maxLeftA, maxLeftB);
-                }
-            } //if median hasnt been found, cut off half of an array
-            else if (maxLeftA > maxLeftB)
-            {
-                end = partitionA - 1;
+        return temp;
+    }
+
+    string longestPalindrome(string s) {
+        string ans,
+               str = fillString(s);
+
+        size_t size = str.size();
+
+        int start = 0,  //iteration from center
+            c = 0,  //center letter of palindrome
+            r = 0,  //rightmost boundary of palindrome
+            max = 0;//highest palindrome found so far   
+
+        //max palindrome of each starting location is stored.
+        vector<int> p(size, 0);
+
+        //for each excluding first and last chars ('$' and '@')
+        for ( int i = 1; i < size - 1; i++ ) {
+            p[i] = 1;
+
+            if ( i < r ) {
+                p[i] = min(r - i, (2 * c - i >= 0) ? p[2 * c - i] : 0);
             }
-            else
-            {
-                start = partitionA + 1;
+
+
+            char minc = ((i - p[i] >= 0 ) ? str[i - p[i] ] : '1'),
+                 pluc = ((i + p[i] < size) ? str[i + p[i]] : '0');
+ 
+            while ( pluc == minc ) {
+                p[i]++;
+                minc = ((i - p[i] >= 0 ) ? str[i - p[i]] : '1');
+                pluc = ((i + p[i] < size) ? str[i + p[i]] : '0');
+            }
+            
+
+            if ( i + p[i] > r ) {
+                c = i;
+                r = i + p[i];
+            }
+            
+            if ( p[i] > max ) {
+                start = (i - p[i]);
+                max = p[i];
             }
         }
 
-        return -1;
+        start = (start + 1) / 2;
+
+        for( int i = start; i <= start + max - 2; i++ )
+            ans += s[i];
+
+        return ans;
     }
 };
