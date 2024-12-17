54fd07dcd78602aa477b6eb48493061e2fae5728
AaronMParish <aaron-m-parish@proton.me>
2024-12-16 22:28:29 -0600

    Added 797 all paths from source to target

---
+++ 797. All Paths From Source To Target/allPathsFromSourceToTarget.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> result;
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> path;
+        dfs( 0, path, graph );
+        return result;
+    }
+
+    void dfs( int current, vector<int> &path, vector<vector<int>>&graph) {
+        path.push_back(current);
+        if ( current == graph.size() - 1 ) {
+            result.push_back(path);
+            path.pop_back();
+            return;
+        }
+
+        for( auto &neighbor : graph[current] ) {
+            dfs( neighbor, path, graph );
+        }
+
+        path.pop_back();
+    }
+};
